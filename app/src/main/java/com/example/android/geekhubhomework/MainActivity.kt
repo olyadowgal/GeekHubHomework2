2afbc6a12fb38f8d045ffa5b37e837493f4461c2
DowgalOlya <dowgalolya@gmail.com>
2018-11-02 19:01:02 +0200

    Add listeners for button_toast and button_count

--- app/src/main/java/com/example/android/geekhubhomework/MainActivity.kt
+++ app/src/main/java/com/example/android/geekhubhomework/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.android.geekhubhomework
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button_toast.setOnClickListener {
+            Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT).show()
+        }
+        button_count.setOnClickListener {
+            val showCountTextView =  findViewById<TextView>(R.id.textView)
+            val countString = showCountTextView.text.toString()
+            var count: Int = Integer.parseInt(countString)
+            count++
+            showCountTextView.text = count.toString()
+        }
     }
 }
